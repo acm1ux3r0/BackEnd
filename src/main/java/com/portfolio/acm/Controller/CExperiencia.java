93e748aab74f6344acd40e4ac686daa2e40317ad
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-05-04 12:25:40 -0300

    Update CExperiencia.java

--- src/main/java/com/portfolio/acm/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/acm/Controller/CExperiencia.java
@@ -23,8 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 /**
  *
  * @author acm1ux3r0
