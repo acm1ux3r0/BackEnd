99fa6427ecf713bff95f5c4339711f975dc5bf4c
Marc0 Lux3r0 <acmluxero@gmail.com>
2023-05-09 22:18:47 -0300

    Se cambia en todos los controllers @CrossOrigin

--- src/main/java/com/portfolio/acm/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/acm/Controller/CExperiencia.java
@@ -30,7 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "*") //http://localhost:4200
+//@CrossOrigin(origins = "*") //http://localhost:4200
+@CrossOrigin(origins = "*")
 
 public class CExperiencia {
     @Autowired //Para inyectar el servicio
--- src/main/java/com/portfolio/acm/Controller/CHys.java
+++ src/main/java/com/portfolio/acm/Controller/CHys.java
@@ -29,7 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin(origins = "*") //http://localhost:4200
+//@CrossOrigin(origins = "*") //http://localhost:4200
+@CrossOrigin(origins = "*")
 @RequestMapping("/hys")
 public class CHys {
 
